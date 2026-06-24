da542183ff66ed5cf71fa92929b908756fa1a27e
Osamah Saeed <engosamahsaeed@gmail.com>
2026-06-25 01:56:57 +0300

    Finish news app

---
+++ News App/news_app/lib/widgets/categories_list_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/models/category_model.dart';
+import 'package:news_app/widgets/category_card.dart';
+
+class CategoriesListView extends StatelessWidget {
+  const CategoriesListView({
+    super.key,
+  });
+
+  final List<CategoryModel> categories = const [
+    CategoryModel(
+      image: 'assets/business.png',
+      categoryName: 'Business',
+      ),
+    CategoryModel(
+      image: 'assets/entertaiment.png',
+      categoryName: 'Entertainment',
+      ),
+    CategoryModel(
+      image: 'assets/health.png', 
+      categoryName: 'Health',
+      ),
+    CategoryModel(
+      image: 'assets/science.png', 
+      categoryName: 'Science',
+      ),
+    CategoryModel(
+      image: 'assets/sports.png', 
+      categoryName: 'Sports',
+      ),
+    CategoryModel(
+      image: 'assets/technology.jpeg', 
+      categoryName: 'Technology',
+      ),
+    CategoryModel(
+      image: 'assets/general.png', 
+      categoryName: 'General',
+      ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 85,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context,index){
+          return  CategoryCard(
+            category: categories[index],
+          );
+        },
+      ),
+    );
+  }
+}
